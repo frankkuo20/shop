273bf02af7dfc41e086e28f1d64e0e6e3e10021c
frankkuo20 <frankkuo20@gmail.com>
2017-12-24 17:38:11 +0800

    choose postgres

--- shop/settings.py
+++ shop/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shopDB',
+        'USER': 'shop',
+        'PASSWORD': 'shoppw',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
