a6183d5bfec6c043257b7fb4054c443e81b18408
frankkuo20 <frankkuo20@gmail.com>
2017-12-31 22:22:40 +0800

    route variable

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from store import views
 
+
+app_name = 'store'
 urlpatterns = [
-    path('category/', views.category),
-    path('categoryCreate/', views.categoryCreate)
+    path('category/', views.category, name='category'),
+    path('categoryCreate/', views.categoryCreate, name='categoryCreate')
 ]
--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.urls import reverse
+
 from store.forms import CategoryForm
 from store.models import Category
 
@@ -19,4 +21,4 @@ def categoryCreate(request):
             return render(request, 'store/categoryCreate.html', {'categoryForm':categoryForm})
 
         categoryForm.save()
-        return redirect('/store/category/')
+        return redirect(reverse('store:category'))
